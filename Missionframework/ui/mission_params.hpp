e5dd54a3e8e17b9261a1ccfbd50930cd12601a19
Carl Mylo <56451149+carlmylo@users.noreply.github.com>
2023-04-05 02:03:03 -0500

    Enable AI Defenders

--- Missionframework/ui/mission_params.hpp
+++ Missionframework/ui/mission_params.hpp
@@ -75,7 +75,7 @@ class Params {
         title = $STR_PARAM_BLUFOR_DEFENDERS;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 0;
+        default = 1;
     };
     class Autodanger {
         title = $STR_PARAM_AUTODANGER;
